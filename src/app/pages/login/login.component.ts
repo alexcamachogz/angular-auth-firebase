5594537e4bad07918b88a6c00021e7e9dba36eb0
Alejandra Camacho <camacho.ale94@gmail.com>
2020-11-19 00:57:41 -0600

    Alerts created

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { NgForm } from '@angular/forms'
+import Swal from 'sweetalert2'
 import { UserModel } from '../../models/user.model'
 import { AuthService } from '../../services/auth.service'
 
@@ -21,12 +22,24 @@ export class LoginComponent implements OnInit {
     if (form.invalid) {
       return
     }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espera por Favor..'
+    })
+    Swal.showLoading()
+
     this.auth.login(this.user).subscribe(
-      (resp) => {
-        console.log(resp)
+      () => {
+        Swal.close()
       },
       (err) => {
-        console.log(err.error.error.message)
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al iniciar sesión',
+          text: err.error.error.message
+        })
       }
     )
   }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { NgForm } from '@angular/forms'
+import Swal from 'sweetalert2'
 import { UserModel } from '../../models/user.model'
 import { AuthService } from '../../services/auth.service'
 
@@ -21,12 +22,24 @@ export class RegistroComponent implements OnInit {
     if (form.invalid) {
       return
     }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espera por Favor..'
+    })
+    Swal.showLoading()
+
     this.auth.newUser(this.user).subscribe(
-      (res) => {
-        console.log(res)
+      () => {
+        Swal.close()
       },
       (err) => {
-        console.log(err.error.error.message)
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al crear cuenta',
+          text: err.error.error.message
+        })
       }
     )
   }
