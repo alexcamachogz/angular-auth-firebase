4756893d20c2d0b840414fb83cc35d02048fafa3
Alejandra Camacho <camacho.ale94@gmail.com>
2020-11-18 19:16:56 -0600

    Init repo

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core'
+import { NgForm } from '@angular/forms'
+import { UserModel } from '../../models/user.model'
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+  user: UserModel
+
+  // constructor() {}
+
+  ngOnInit() {
+    this.user = new UserModel()
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return
+    }
+    console.log(this.user)
+    console.log(form)
+  }
+}
